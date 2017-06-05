f641cdee35658eff45afe7a70936a9fb6a040104
Devi <Devi@Devi-PC>
2017-06-05 09:42:19 +0530

    Initial commit

---
+++ src/main/java/com/cc/referralapp/ReferralappApplication.java
@@ -0,0 +1,12 @@
+package com.cc.referralapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferralappApplication.class, args);
+	}
+}
